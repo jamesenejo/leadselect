ed2f42f2297a684fcf59d247e026791a4be41776
James Enejo <james.enejo@andela.com>
2018-08-25 12:52:54 +0100

    update selectionController function

--- server/controllers/selectionController.js
+++ server/controllers/selectionController.js
@@ -3,14 +3,11 @@ import models from '../models/index';
 const { Leaders } = models;
 
 const selectionController = {
-    getCurrent: (req, res) => {
-        Leaders
-            .findById(1)
-            .then(data => res.status(200).json({
-                status: 'success',
-                data
-            }));
-    }
+    getCurrent: (req, res) => Leaders.findById(1)
+        .then(data => res.status(200).json({
+            status: 'success',
+            data
+        }))
 };
 
 export default selectionController;
